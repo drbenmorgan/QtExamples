60773bb3f36cfed2b25b1f56ad7639974f791a92
Ben Morgan <Ben.Morgan@warwick.ac.uk>
2016-07-02 16:23:55 +0100

    Add Qdir/QResource examples to Filesystem
    Use QDir prefix and search paths to demonstrate use of a
    layered configuration lookup with separation via prefixes.
    
    Generate an RCC QResource file and demonstrate that this can
    be mounted and added into the QDir searchpath for files.

--- Filesystem/qt-filesystem.cpp
+++ Filesystem/qt-filesystem.cpp
@@ -16,25 +16,61 @@
 // You should have received a copy of the GNU General Public License
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+#include <QCoreApplication>
 #include <QDir>
+#include <QResource>
 #include <QDebug>
 
 int main(int argc, char *argv[]) {
+  QCoreApplication app(argc, argv);
 
-  // Get list of search paths for null prefix
-  QStringList searchPaths(QDir::searchPaths("/"));
-
-  Q_FOREACH(const QString& p, searchPaths)
+  qDebug() << "Default application library paths:";
+  Q_FOREACH(const QString& p, app.libraryPaths())
       qDebug() << p;
 
-  // Add a search path
-  QDir::setSearchPaths("falaise", QStringList(QDir::homePath() + "/.falaise"));
+  // Load the default resource bundle
+  QResource::registerResource(QCoreApplication::applicationDirPath() + "/falaise.rcc");
 
-  searchPaths = QDir::searchPaths("falaise");
+  // Add Search paths for "falaise" prefix
+  // The order affects (later) how files are located. Consider it a layered
+  // configuration/
+  QDir::setSearchPaths("falaise",
+                       QStringList() << ":/falaise"
+                                     << QCoreApplication::applicationDirPath() + "/config"
+                                     << QCoreApplication::applicationDirPath() + "/user"
+                       );
+  // Add search paths for "bayeux" prefix. This means we can have identical
+  // filenames, but reoslved differently according to prefix.
+  QDir::setSearchPaths("bayeux",
+                       QStringList() << QCoreApplication::applicationDirPath() + "/bayeux"
+                       );
 
-  Q_FOREACH(const QString& p, searchPaths)
+
+  qDebug() << "Default file search paths for 'falaise' prefix:";
+
+  Q_FOREACH(const QString& p, QDir::searchPaths("falaise"))
       qDebug() << p;
 
+  // See how files are resolved
+  QString configFilename("config.conf");
+
+  // In Falaise, it should be the one in config
+  qDebug() << "Which falaise config file is found first?:";
+  QFile falaiseConfig("falaise:" + configFilename);
+  if(falaiseConfig.exists()) {
+    falaiseConfig.open(QIODevice::ReadOnly);
+    QTextStream in(&falaiseConfig);
+    qDebug() << falaiseConfig.fileName() << ": " << in.readLine();
+  }
+
+  // Now qualify with bayeux prefix
+  qDebug() << "Is Bayeux config file resolved?:";
+  QFile bayeuxConfig("bayeux:" + configFilename);
+  if(bayeuxConfig.exists()) {
+    bayeuxConfig.open(QIODevice::ReadOnly);
+    QTextStream in(&bayeuxConfig);
+    qDebug() << bayeuxConfig.fileName() << ": " << in.readLine();
+  }
 
   return 0;
 }
